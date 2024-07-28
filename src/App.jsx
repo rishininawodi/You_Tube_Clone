e020f2c97b1fc42d505dc2412395db5960e13d94
chamudi <chamuvithanage@gmail.com>
2024-07-28 14:32:20 +0530

    created nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Avatar from 'react-avatar';
+import { AiOutlineMenu } from "react-icons/ai";
+import { CiSearch } from "react-icons/ci";
+import { IoMdMic } from "react-icons/io";
+import { RiVideoAddLine } from "react-icons/ri";
+import { AiOutlineBell } from "react-icons/ai";
+
+import logo from "../../public/logo.png";
+import profile from"../../public/profile.jpg";
+function Navbar() {
+  return (
+    <div className="flex justify-between px-6 py-2">
+      
+      <div className="flex items-center space-x-4 ">
+        <AiOutlineMenu className="text-xl cursor-pointer"/> 
+        <img src={logo} alt="" className="w-28 cursor-pointer"/>
+      </div>
+      
+      <div className="flex w-[35%] items-center">
+        <div className="w-[100%] px-3 py-2 border rounded-l-full">
+            <input type="text" placeholder="Search" className="outline-none" />
+        </div>
+        <button className="px-4 py-2 border bg-gray-100 rounded-r-full">
+            <CiSearch size={"24px"}/>
+        </button>
+        
+        <IoMdMic size={"42px"} className="ml-3 border rounded-full py-2 cursor-pointer hover:bg-gray-200 duration-200 "/>
+        
+      </div>
+      <div className="flex space-x-5  items-center ">
+        <RiVideoAddLine className="text-2xl"/>
+        <AiOutlineBell className="text-2xl"/>
+        <Avatar src={profile} size="32" round={true}/>
+
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
