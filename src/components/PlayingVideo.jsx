f9e1870acbf2333095f5aebf8919f1b57ddf82a6
Rishini <nawodirishini88@gmail.com>
2024-08-24 13:46:22 +0530

    add vedio subscrribe,desc,comment part

--- src/components/PlayingVideo.jsx
+++ src/components/PlayingVideo.jsx
@@ -2,6 +2,9 @@ import React, { useEffect,useState } from 'react';
 import { useParams } from 'react-router-dom';
 import {fetchData} from "../utils/rapidapi";
 import ReactPlayer from "react-player";
+import { AiOutlineLike } from 'react-icons/ai';
+import { BsFillCheckCircleFill } from 'react-icons/bs';
+
 
 const abbreviateNumber = (number, digits) => {
   const SI_SYMBOL = ["", "k", "M", "G", "T", "P", "E"];
@@ -44,7 +47,53 @@ function PlayingVideo() {
             style={{backgroundColor:"#000000"}}
             playing={true}
 
-            />
+            /> 
+          </div>
+          <div className="font-bold text-sm md:text-xl mt-4 line-clamp-2">
+            {video?.title}
+          </div>
+          <div className="flex justify-between flex-col md:flex-row mt-4">
+            <div className="flex ">
+              <div className="flex items-start">
+                <div className="flex h-11 w-11 rounded-full overflow-hidden">
+                  <img
+                    className="h-full w-full object-cover"
+                    src={video?.author?.avatar[0]?.url}
+                  />
+                </div>
+              </div>
+              <div className="flex space-x-5">
+                <div className="flex flex-col ml-3">
+                  <div className="text-md font-semibold flex items-center">
+                    {video?.author?.title}
+                    {video?.author?.badges[0]?.type === "VERIFIED_CHANNEL" && (
+                      <BsFillCheckCircleFill className="text-white/[0.5] text-[12px] ml-1" />
+                    )}
+                  </div>
+                  <div className=" text-sm">
+                    {video?.author?.stats?.subscribersText}
+                  </div>
+                </div>
+                <span className="mt-1 text-center bg-red-500 px-3 pt-2 rounded-full text-white cursor-pointer hover:bg-red-700 duration-200 ">
+                  Subscribe
+                </span>
+              </div>
+            </div>
+            <div className="flex mt-4 md:mt-0">
+              <div className="flex items-center justify-center h-11 px-6 rounded-3xl bg-white/[0.15]">
+                <AiOutlineLike className="text-xl mr-2" />
+                {`${abbreviateNumber(video?.stats?.likes, 2)} Likes`}
+              </div>
+              <div className="flex items-center justify-center h-11 px-6 rounded-3xl bg-white/[0.15] ml-4">
+                {`${abbreviateNumber(video?.stats?.views, 2)} Views`}
+              </div>
+            </div>
+          </div>
+          <div className="p-4 bg-gray-100 rounded-xl mt-4 text-sm">
+            {video?.description}
+          </div>
+          <div className="flex gap-x-6 font-semibold rounded-xl mt-4 text-xl">
+            {video?.stats?.comments} <p>Comments</p>
           </div>
           <div>
             <span className='text-sm fonnt-bold line-clamp-2'>
